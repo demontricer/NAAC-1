61b71d7c6ee9d05b30398f9ab0d97a8dbbb29edc
BreethiSundar <66816394+BreethiSundar@users.noreply.github.com>
2020-06-12 13:23:32 +0530

    Update utility.php

--- utility.php
+++ utility.php
@@ -1,3 +1,13 @@
 <?php
-  
+  function utility($str)
+  {
+    if(isset($_POST[$str]) || isset($_GET[$str]))
+       {
+          $str1=$str;   
+       }
+    else
+       {
+          $str1='null';
+       }
+  }
 ?>
