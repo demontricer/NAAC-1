d6bbd68df44b12cc3ce7921a5807232d737f80d9
BreethiSundar <66816394+BreethiSundar@users.noreply.github.com>
2020-06-12 11:37:16 +0530

    Update naac.sql

--- naac.sql
+++ naac.sql
@@ -1,1 +1,2 @@
-
+Create table AUTH_USER(NAAC_USER_ID int,NAAC_USERNAME varchar(30),NAAC_PASSWORD varchar(30),NAAC_ROLE varchar(30));
+                                                                                                            
